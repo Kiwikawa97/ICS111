720dee4f2db6c8b00007c4fbeafef335b3482677
RileyMiyamoto <rkam9@hawaii.edu>
2018-10-09 10:31:39 -1000

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -1,6 +1,6 @@
 public class StudentTest{
    public static void main(String[] args){
-      //create Student object
+      //create Student object (need first name, last name, id, and gpa)
       
       //print out each property of the student object
       
