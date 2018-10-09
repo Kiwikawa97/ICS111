88c70d3c015e66b47ea526725ba4cdbffb483b20
RileyMiyamoto <rkam9@hawaii.edu>
2018-10-09 09:33:54 -1000

    Add files via upload

---
+++ StudentTest.java
@@ -0,0 +1,11 @@
+public class StudentTest{
+   public static void main(String[] args){
+      //create Student object
+      
+      //print out each property of the student object
+      
+      //modify the student object
+      
+      //print modified student object using toString method
+   }
+}
