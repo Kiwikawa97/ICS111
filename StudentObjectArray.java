89cc4cc9f076e2a65f437966a2f04aeac54d35fc
RileyMiyamoto <rkam9@hawaii.edu>
2018-10-11 10:32:54 -1000

    Add files via upload

---
+++ StudentObjectArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+public class StudentObjectArray{
+   public static void main(String[] args) throws FileNotFoundException{
+      String fName, lName;
+      int id;
+      double gpa;
+      int i = 0;
+      //Create scanner object and read from file 'Student.txt'
+      
+      //Create an array of objects of size 4
+            
+      //Use a while loop to save file input into variables
+
+         //Create an object for each item from file
+   
+      //Use a for loop to print out each object
+   }
+}
