1da24c781af4b247f3e52092b7087d215355ad51
RileyMiyamoto <rkam9@hawaii.edu>
2018-10-25 16:44:00 -1000

    Add files via upload

---
+++ Work.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Work{
+   public static void main(String[] args) throws IOException{
+      while(){
+         
+      }
+      switch(choice){
+         case 4: 
+         case 3:          
+         case 2: 
+         case 1: 
+         case 0: 
+         default: 
+      }
+   }
+}
