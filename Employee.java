c1213bb2868e66d637bcc2a85cb4f540713f36e6
RileyMiyamoto <rkam9@hawaii.edu>
2018-10-25 14:12:58 -1000

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,36 @@
+public class Employee{
+   
+  public Employee(){
+     
+  }
+  public void setNum(){
+     
+  }
+  public int getNum(){
+     
+  }
+  public void setfName(){
+  
+  } 
+  public String getfName(){
+  
+  }
+  public void setlName(){
+  
+  }
+  public String getlName(){
+  
+  }
+  public void setHourly(){
+  
+  }
+  public double getHourly(){
+    
+  }
+  public String toString(){
+      return output;
+  }
+  public double weeklyPay(){
+      return pay;
+  }
+}
